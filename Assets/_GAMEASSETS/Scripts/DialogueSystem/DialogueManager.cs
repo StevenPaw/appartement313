db5a3dd83e2c796a40d5350001e7fa349f9bae98
StevenPaw <stevenpaw209@gmail.com>
2021-07-05 21:25:53 +0200

    [ADDED] UI for dialogue

---
+++ Assets/_GAMEASSETS/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    private void Start()
+    {
+        sentences = new Queue<string>();
+    }
+}
